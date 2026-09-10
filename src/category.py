9f51592c45c254b18a20b9492ee471dcd46b8b9f
Анастасия Варго <avvargo@gmail.com>
2026-09-10 22:53:34 +0300

    Add Product and Category classes

---
+++ src/category.py
@@ -0,0 +1,14 @@
+class Category:
+    """Класс для категорий"""
+
+    # Переменная на уровне класса для подсчета количества категорий
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name:str, description:str, products:list[Product]):
+
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
